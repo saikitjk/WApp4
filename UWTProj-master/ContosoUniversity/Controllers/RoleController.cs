4d7ac4cfd7dd8e4dddf076d6a562de9fc97fb827
Jason Saikit Kwong <saikitjk@gmail.com>
2018-03-07 13:11:41 -0800

    First Commit

---
+++ UWTProj-master/ContosoUniversity/Controllers/RoleController.cs
@@ -0,0 +1,103 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        // GET: Role
+        public ActionResult Index()
+        {
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        // GET: Role/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.Roles.Add(Role);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
